760155c9734cc3c9ff36bf4779a97a50636a5623
Natalia Sugrobova <natalyasugrobova@gmail.com>
2024-08-10 00:00:49 +0200

    Updating js to add functionality (bonus)

--- Exercise4_shopping_list/main.js
+++ Exercise4_shopping_list/main.js
@@ -3,11 +3,25 @@ const listElements = document.querySelector("ul");
 
 buttonAdd.addEventListener("click", function(){
     let liElement = document.createElement('li');
-    liElement.innerText = prompt("Enter a product you want to add: ");
+    liElement.style.display = 'flex';
+    liElement.style.alignItems = 'center';
+
+    let checkBox = document.createElement("input");
+    checkBox.type = 'checkbox';
+    checkBox.style.marginRight = '10px';
+
+    let itemName = prompt("Enter a product you want to add: ");
+    liElement.innerHTML = `${checkBox.outerHTML} ${itemName} `;
+
+    let deleteIcon = document.createElement('span');
+    deleteIcon.innerHTML = '🗑️';
+    deleteIcon.style.cursor = 'pointer';
+    deleteIcon.style.marginLeft = '10px';
+    liElement.appendChild(deleteIcon);
+
     listElements.append(liElement);
     
-    liElement.addEventListener('click', function(){
+    deleteIcon.addEventListener('click', function(){
         liElement.remove();
     });
 });
-
